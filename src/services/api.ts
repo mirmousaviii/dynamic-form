1010c93ce6da46ae1a320bf552b3f98f2cc191c5
Mostafa Mirmousavi <mirmousavi.m@gmail.com>
2024-05-02 12:13:15 +0200

    feat: use .env for API_URL

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
+const API = process.env.EXPO_PUBLIC_API_URL;
+
 export const fetchFormData = async () => {
-    const response = await fetch('https://dynamic-form.wiremockapi.cloud/api/v1/form');
+    const response = await fetch(`${API}/form`);
     const data = await response.json();
     return data;
 };
